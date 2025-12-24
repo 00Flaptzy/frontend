cdc3e28144ded7bcedfaf8ce3c8eb92a835bcf67
Black-EDG <yhurycristiamanampaquispe@gmail.com>
2025-12-24 18:53:03 -0500

    Actualización: agregando .env y urls.jsx corregido

--- src/api_fetching/urls.jsx
+++ src/api_fetching/urls.jsx
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 // AUTHORIZATION
 export const testURL = BASE_URL;
 export const loginURL = `${BASE_URL}/login`;
