503bda255b71b8dc33515e9465d28181801399b0
Black-EDG <yhurycristiamanampaquispe@gmail.com>
2025-12-24 19:01:07 -0500

    Actualización: agregando .env y urls.jsx corregido

--- src/api_fetching/urls.jsx
+++ src/api_fetching/urls.jsx
@@ -1,21 +1,33 @@
+// =========================
+// urls.jsx
+// =========================
+
 const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+// =========================
 // AUTHORIZATION
-export const testURL = BASE_URL;
-export const loginURL = `${BASE_URL}/login`;
-export const registerURL = `${BASE_URL}/register`;
-export const logoutURL = `${BASE_URL}/logout`;
-export const getUserProfileURL = `${BASE_URL}/get_user_profile`;
-export const changeUsernameURL = `${BASE_URL}/change_username`;
-export const changePasswordURL = `${BASE_URL}/change_password`;
-export const checkTokenExpiery = `${BASE_URL}/check_token`;
+// =========================
+export const testURL = `${BASE_URL}/auth/`; // prueba
+export const loginURL = `${BASE_URL}/auth/login`;
+export const registerURL = `${BASE_URL}/auth/register`;
+export const logoutURL = `${BASE_URL}/auth/logout`;
+export const getUserProfileURL = `${BASE_URL}/auth/get_user_profile`;
+export const changeUsernameURL = `${BASE_URL}/auth/change_username`;
+export const changePasswordURL = `${BASE_URL}/auth/change_password`;
+export const checkTokenExpiery = `${BASE_URL}/auth/check_token`;
 
-// MAIN FUNCTIONALITY
-export const addHabitURL = `${BASE_URL}/add_habit`;
-export const getHabitsURL = `${BASE_URL}/get_habits`;
-export const habitCompletionURL = `${BASE_URL}/habit_completion`;
-export const deleteHabitURL = `${BASE_URL}/delete_habit`;
-export const getHabitCompletionsURL = `${BASE_URL}/get_habit_completions`;
-export const getAllCompletionsURL = `${BASE_URL}/get_all_completions`;
-export const uncompleteHabitURL = `${BASE_URL}/uncomplete_habit`;
+// =========================
+// MAIN FUNCTIONALITY - HABITS
+// =========================
+export const addHabitURL = `${BASE_URL}/habits/add_habit`;
+export const getHabitsURL = `${BASE_URL}/habits/get_habits`;
+export const habitCompletionURL = `${BASE_URL}/habits/habit_completion`;
+export const deleteHabitURL = `${BASE_URL}/habits/delete_habit`;
+export const getHabitCompletionsURL = `${BASE_URL}/habits/get_habit_completions`;
+export const getAllCompletionsURL = `${BASE_URL}/habits/get_all_completions`;
+export const uncompleteHabitURL = `${BASE_URL}/habits/uncomplete_habit`;
 
-export const getUNIXFromMidnightURL = `${BASE_URL}/get_UNIX_from_midnight`;
+// =========================
+// UTILS
+// =========================
+export const getUNIXFromMidnightURL = `${BASE_URL}/utils/get_UNIX_from_midnight`;
